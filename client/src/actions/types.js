3c6e60ac9f4409b949ebe0ad18d49492662d8851
Łukasz Leni Zembrowski <zembro@gmail.com>
2020-09-08 16:42:59 +0200

    Add types for actions

---
+++ client/src/actions/types.js
@@ -0,0 +1,4 @@
+export const SET_GAME_SETTINGS = "SET_GAME_SETTINGS";
+export const SET_GAME_ROUNDS = "SET_GAME_ROUNDS";
+export const GAME_SET_OVER = "GAME_SET_OVER";
+export const CLEAR_GAME_SET_OVER = "CLEAR_GAME_SET_OVER";
