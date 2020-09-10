61c77966ac3b6b4ba22f5395c28c8004c7dfe4b1
Łukasz Leni Zembrowski <zembro@gmail.com>
2020-09-10 15:51:41 +0200

    Add game save and match over

--- client/src/reducers/game.js
+++ client/src/reducers/game.js
@@ -2,11 +2,19 @@ import {
   SET_GAME_ROUNDS,
   CLEAR_GAME_SET_OVER,
   GAME_SET_OVER,
+  GAME_SAVE_SETS,
+  MATCH_OVER,
 } from "../actions/types";
 
 const initialState = {
   gameRounds: [],
   gameSetOver: false,
+  gameSaveSets: [],
+  matchOver: false,
+  won: {
+    teamOne: false,
+    teamTwo: false,
+  },
 };
 
 export default function (state = initialState, action) {
@@ -18,8 +26,15 @@ export default function (state = initialState, action) {
         ...state,
         gameRounds: [...state.gameRounds, payload],
       };
+    case GAME_SAVE_SETS:
+      const gameRounds = state.gameRounds;
+      return {
+        ...state,
+        gameSaveSets: [...state.gameSaveSets, gameRounds],
+      };
     case GAME_SET_OVER:
       return {
+        ...state,
         gameRounds: [],
         gameSetOver: true,
       };
@@ -27,6 +42,20 @@ export default function (state = initialState, action) {
       return {
         ...state,
         gameSetOver: false,
+      };
+    case MATCH_OVER:
+      let winner = {};
+      if (payload === "one") {
+        winner = { teamOne: true, teamTwo: false };
+      } else if (payload === "two") {
+        winner = { teamOne: false, teamTwo: true };
+      }
+
+      return {
+        ...state,
+        gameRounds: [],
+        matchOver: true,
+        won: winner,
       };
 
     default:
