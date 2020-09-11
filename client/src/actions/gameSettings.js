a4519886c6cdbf88d54c625545e8cd28523fed6d
Łukasz Leni Zembrowski <zembro@gmail.com>
2020-09-11 19:28:33 +0200

    Add new actions

--- client/src/actions/gameSettings.js
+++ client/src/actions/gameSettings.js
@@ -1,8 +1,13 @@
-import { SET_GAME_SETTINGS } from "./types";
+import { SET_GAME_SETTINGS, NEW_GAME } from "./types";
 
-export const gameStart = (teamOne, teamTwo, maxPoints) => (dispatch) => {
+export const gameStart = (teamOne, teamTwo, maxPoints, ballServers,bestOf) => (
+  dispatch
+) => {
   dispatch({
     type: SET_GAME_SETTINGS,
-    payload: { teamOne, teamTwo, maxPoints },
+    payload: { teamOne, teamTwo, maxPoints, ballServers ,bestOf},
   });
+};
+export const newGame = () => (dispatch) => {
+  dispatch({ type: NEW_GAME });
 };
--- client/src/reducers/gameSettings.js
+++ client/src/reducers/gameSettings.js
@@ -1,17 +1,17 @@
-import { SET_GAME_SETTINGS } from "../actions/types";
+import { SET_GAME_SETTINGS, NEW_GAME } from "../actions/types";
 
 const initialState = {
   teamOne: {
-    playerOne: { name: "Rudy", role: "server", id: 1 },
-    playerTwo: { name: "Rudy2", role: "player", id: 2 },
+    playerOne: { name: "1", server: true, id: 1 },
+    playerTwo: { name: "2", server: false, id: 2 },
   },
   teamTwo: {
-    playerOne: { name: "Łysy", role: "player", id: 3 },
-    playerTwo: { name: "Łysy2", role: "player", id: 4 },
+    playerOne: { name: "3", server: true, id: 3 },
+    playerTwo: { name: "4", server: false, id: 4 },
   },
-  maxPoints: 6,
-  bestOf: 2,
-  gameBegun: true,
+  maxPoints: null,
+  bestOf: null,
+  gameBegun: false,
 };
 
 export default function (state = initialState, action) {
@@ -19,15 +19,53 @@ export default function (state = initialState, action) {
 
   switch (type) {
     case SET_GAME_SETTINGS:
-      const { teamOne, teamTwo, maxPoints } = payload;
+      const { teamOne, teamTwo, maxPoints, ballServers, bestOf } = payload;
+      console.log(teamOne);
       return {
         ...state,
-        teamOne: teamOne,
-        teamTwo: teamTwo,
+        teamOne: {
+          playerOne: {
+            name: teamOne.playerOne,
+            server: ballServers.teamOnePlayerOne,
+            id: 1,
+          },
+          playerTwo: {
+            name: teamOne.playerTwo,
+            server: ballServers.teamOnePlayerTwo,
+            id: 2,
+          },
+        },
+        teamTwo: {
+          playerOne: {
+            name: teamTwo.playerOne,
+            server: ballServers.teamTwoPlayerOne,
+            id: 3,
+          },
+          playerTwo: {
+            name: teamTwo.playerTwo,
+            server: ballServers.teamTwoPlayerTwo,
+            id: 4,
+          },
+        },
+        bestOf: bestOf,
         maxPoints: maxPoints,
         gameBegun: true,
       };
-
+    case NEW_GAME:
+      return {
+        ...state,
+        gameBegun: false,
+        maxPoints: null,
+        bestOf: null,
+        teamOne: {
+          playerOne: { name: "", server: false, id: 1 },
+          playerTwo: { name: "", server: false, id: 2 },
+        },
+        teamTwo: {
+          playerOne: { name: "", server: false, id: 3 },
+          playerTwo: { name: "", server: false, id: 4 },
+        },
+      };
     default:
       return state;
   }
