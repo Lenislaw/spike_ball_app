9569844fcc90bbbbbf358eda17c2f77346ca9b1b
Łukasz Leni Zembrowski <zembro@gmail.com>
2020-09-14 15:32:03 +0200

    Change Servers Componenet

---
+++ client/src/Components/SpikeballCounter/gameBoard/game/ChangeServers.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+const ChangeServers = ({
+  onChangeTeamOne,
+  TeamOnePlayerOne,
+  TeamOnePlayerTwo,
+  onChangeTeamTwo,
+  TeamTwoPlayerOne,
+  TeamTwoPlayerTwo,
+  closeBox,
+}) => {
+  return (
+    <div className="change-servers">
+      <div className="team-one">
+        <h3>Team One</h3>
+        <div className="inputs">
+          <div className="select">
+            <h3>Select first server in next set!</h3>
+            <select
+              name="select-server-team-one"
+              id="select-server-team-one"
+              onChange={onChangeTeamOne}
+            >
+              <option id="server-teamOne-playerOne" value={TeamOnePlayerOne}>
+                {TeamOnePlayerOne}
+              </option>
+              <option id="server-teamOne-playerTwo" value={TeamOnePlayerTwo}>
+                {TeamOnePlayerTwo}
+              </option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div className="team-two">
+        <h3>Team Two</h3>
+        <div className="inputs">
+          <div className="select">
+            <select
+              name="select-server-team-two"
+              id="select-server-team-two"
+              onChange={onChangeTeamTwo}
+            >
+              <option id="server-teamTwo-playerOne" value={TeamTwoPlayerOne}>
+                {TeamTwoPlayerOne}
+              </option>
+              <option id="server-teamTwo-playerTwo" value={TeamTwoPlayerTwo}>
+                {TeamTwoPlayerTwo}
+              </option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div className="button">
+        <button onClick={closeBox}>Close</button>
+      </div>
+    </div>
+  );
+};
+
+export default ChangeServers;
