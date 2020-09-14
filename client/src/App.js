cc1bea426f5efe9e9f03f5595339ed31b32be01d
Łukasz Leni Zembrowski <zembro@gmail.com>
2020-09-14 15:33:28 +0200

    Update App and app styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import Nav from "./Components/layout/Nav/Nav";
 import SpikeBallCounter from "./Components/SpikeballCounter/SpikeBallCounter";
+import DarkModeToggle from "./Components/DarkModeToggle/DarkModeToggle.js";
 
 //Redux
 import { Provider } from "react-redux";
 import store from "./store";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(getInitialMode());
+
+  useEffect(() => {
+    localStorage.setItem("dark", JSON.stringify(darkMode));
+  }, [darkMode]);
+
+  function getInitialMode() {
+    const isReturningUser = "dark" in localStorage;
+    const savedMode = JSON.parse(localStorage.getItem("dark"));
+    const userPrefDark = getPrefColorScheme();
+
+    if (isReturningUser) {
+      return savedMode;
+    } else if (userPrefDark) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  function getPrefColorScheme() {
+    if (!window.matchMedia) return;
+    return window.matchMedia("(prefers-color-scheme:dark)").matches;
+  }
+
   return (
     <Provider store={store}>
-      <div className="App">
-        Spike ball App
-        <SpikeBallCounter />
+      <div className={darkMode ? "App dark-mode" : "App light-mode"}>
+        {/* <Nav /> */}
+        <DarkModeToggle darkMode={darkMode} setDarkMode={setDarkMode} />
+
+        <SpikeBallCounter darkMode={darkMode} />
       </div>
     </Provider>
   );
